35f328159061f7866d5a92cd1c9db796cdd3204a
Ben Langfeld <ben@langfeld.me>
2012-08-19 21:51:01 +0100

    Allow adding misc dialplan contexts via data bags

--- recipes/default.rb
+++ recipes/default.rb
@@ -51,6 +51,7 @@ end
 external_ip = node[:ec2] ? node[:ec2][:public_ipv4] : node[:ipaddress]
 users = search(:asterisk_users) || []
 auth = search(:auth, "id:google") || []
+dialplan_contexts = search(:asterisk_contexts) || []
 
 %w{sip manager modules extensions gtalk jabber}.each do |template_file|
   template "/etc/asterisk/#{template_file}.conf" do
@@ -58,7 +59,7 @@ auth = search(:auth, "id:google") || []
     owner "asterisk"
     group "asterisk"
     mode 0644
-    variables :external_ip => external_ip, :users => users, :auth => (auth[0] || {})
+    variables :external_ip => external_ip, :users => users, :auth => (auth[0] || {}), :dialplan_contexts => dialplan_contexts
     notifies :reload, resources(:service => "asterisk")
   end
 end
