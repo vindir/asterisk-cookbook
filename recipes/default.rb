f102321148577bb045a68313a3747b7b503c49c5
Luca Pradovera <luca.pradovera@gmail.com>
2013-04-15 11:09:13 +0200

    Added recipe for installing Asterisk from source

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,27 +17,6 @@
 # limitations under the License.
 #
 
-case node['platform']
-when "ubuntu","debian"
-  if node['asterisk']['use_digium_repo']
-    apt_repository "asterisk" do
-      uri "http://packages.asterisk.org/deb"
-      components ["lucid", "main"]
-      action :add
-    end
-
-    execute "update-asterisk-repo" do
-      command "apt-get update"
-    end
-  end
-
-  node['asterisk']['packages'].each do |pkg|
-    package pkg do
-      options "--force-yes"
-    end
-  end
-end
-
 service "asterisk" do
   supports :restart => true, :reload => true, :status => :true, :debug => :true,
     "logger-reload" => true, "extensions-reload" => true,
---
+++ recipes/source.rb
@@ -0,0 +1,35 @@
+case node['platform']
+when "ubuntu","debian"
+  node['asterisk']['source']['packages'].each do |pkg|
+    package pkg do
+      options "--force-yes"
+    end
+  end
+end
+
+# http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-11.3.0.tar.gz
+
+remote_file "/usr/local/src/asterisk-#{node['asterisk']['source']['version']}.tar.gz" do
+  source "http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-#{node['asterisk']['source']['version']}.tar.gz"
+end
+
+bash "prepare_dir" do
+  user "root"
+  cwd "/usr/local/src"
+  code <<-EOH
+    tar -zxf asterisk-#{node['asterisk']['source']['version']}.tar.gz
+  EOH
+end
+
+bash "install_asterisk" do
+  user "root"
+  cwd "/usr/local/src/asterisk-#{node['asterisk']['source']['version']}"
+  code <<-EOH
+    ./configure
+    make
+    make install
+    make config
+    make samples
+  EOH
+  notifies :reload, resources(:service => "asterisk")
+end
