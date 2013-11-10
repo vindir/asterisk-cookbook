0968f00790177fa0fd95fe63984b66741225d68c
Ben Langfeld <ben@langfeld.me>
2013-11-10 20:33:56 -0200

    Ditch all config for google talk

--- recipes/config.rb
+++ recipes/config.rb
@@ -1,5 +1,4 @@
 users = search(:asterisk_users) || []
-auth = search(:auth, "id:google") || []
 dialplan_contexts = search(:asterisk_contexts) || []
 
 template "#{node['asterisk']['prefix']['conf']}/asterisk/asterisk.conf" do
@@ -12,7 +11,7 @@ end
   template "/etc/asterisk/#{template_file}.conf" do
     source "#{template_file}.conf.erb"
     mode 0644
-    variables :users => users, :auth => auth[0], :dialplan_contexts => dialplan_contexts
+    variables :users => users, :dialplan_contexts => dialplan_contexts
     notifies :reload, resources('service[asterisk]')
   end
 end
