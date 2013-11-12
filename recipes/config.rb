39b18161f21aef99d70636319ed07c12e63a9a08
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2013-11-12 11:38:48 -0700

    Fix handling of the aterisk.prefix.conf attribute

--- recipes/config.rb
+++ recipes/config.rb
@@ -1,14 +1,17 @@
 users = data_bag(:asterisk_users) || []
 dialplan_contexts = data_bag(:asterisk_contexts) || []
+config_dir = "#{node['asterisk']['prefix']['conf']}/asterisk"
 
-template "#{node['asterisk']['prefix']['conf']}/asterisk/asterisk.conf" do
+directory config_dir
+
+template "#{config_dir}/asterisk.conf" do
   source 'asterisk.conf.erb'
   mode 0644
   notifies :reload, resources('service[asterisk]')
 end
 
 %w{sip manager modules extensions}.each do |template_file|
-  template "/etc/asterisk/#{template_file}.conf" do
+  template "#{config_dir}/#{template_file}.conf" do
     source "#{template_file}.conf.erb"
     mode 0644
     variables :users => users, :dialplan_contexts => dialplan_contexts
