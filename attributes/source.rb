f7f981eb416cda77a562125ac8a85fbc65089339
Ben Langfeld <ben@langfeld.me>
2013-12-10 12:04:15 -0500

    Install Asterisk 11.6.0 by default
    Asterisk publishes SHA1 checksums of releases, but Chef only supports SHA256 right now (https://tickets.opscode.com/browse/CHEF-3634)

--- attributes/source.rb
+++ attributes/source.rb
@@ -1,6 +1,6 @@
 default['asterisk']['source']['packages'] = %w{build-essential libssl-dev libcurl4-openssl-dev libncurses5-dev libnewt-dev libxml2-dev libsqlite3-dev uuid-dev}
-default['asterisk']['source']['version']  = '11.5.1'
-default['asterisk']['source']['checksum'] = 'fefa9def9c8f97c89931f12b29b3ac616ae1a8454c01c524678163061dcb42b2'
+default['asterisk']['source']['version']  = '11.6.0'
+default['asterisk']['source']['checksum'] = '80aa523bdaab71ce419022d8c851aaeda718dc5bbf5fe8cb99786da546dd8cee'
 
 # An full download url can be supplied to specify an alternative source tarball location
 default['asterisk']['source']['url'] = nil
