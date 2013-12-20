4fd3ff7bdba5cd6d71f82c5aa73fae754c5fb7c3
Ben Langfeld <ben@langfeld.me>
2013-12-20 15:55:25 -0200

    Bump to 0.2.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@mojolingo.com'
 license          'Apache 2.0'
 description      'Installs/Configures Asterisk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.5'
+version          '0.2.6'
 
 recipe 'asterisk', 'Install Asterisk and configure'
 recipe 'asterisk::unimrcp', 'Install Asterisk UniMRCP plugin and configure'
