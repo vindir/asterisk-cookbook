de57876a901ed40281794386e4b8e48d98618e2b
Jamey Owens <jowens@mojolingo.com>
2014-01-07 15:45:34 -0500

    Update extensions.conf template to set up the [general] config using chef attributes

---
+++ attributes/extensions.rb
@@ -0,0 +1,5 @@
+default['asterisk']['extensions']['static']              = 'yes'
+default['asterisk']['extensions']['writeprotect']        = 'no'
+default['asterisk']['extensions']['autofallthrough']     = 'yes'
+default['asterisk']['extensions']['clearglobalvars']     = 'no'
+default['asterisk']['extensions']['priorityjumping']     = 'no'
